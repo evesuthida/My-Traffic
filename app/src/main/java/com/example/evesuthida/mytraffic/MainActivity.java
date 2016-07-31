7fa340797aebe438df4468e1346ee92095b589e0
suthida <zarasale@gmail.com>
2016-07-31 15:10:58 +0700

    Create DetailActivity Finish 31 Jul 16

--- app/src/main/java/com/example/evesuthida/mytraffic/MainActivity.java
+++ app/src/main/java/com/example/evesuthida/mytraffic/MainActivity.java
@@ -6,6 +6,7 @@ import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
@@ -56,6 +57,14 @@ public class MainActivity extends AppCompatActivity {
         //Create Listview
         MyAdapter myAdapter = new MyAdapter(this, iconInts, titleStrings, detailShort);
         listView.setAdapter(myAdapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Intent intent = new Intent(MainActivity.this, DetailActivity.class);
+                startActivity(intent);
+            }
+        });
 
     } //Main Method
 
