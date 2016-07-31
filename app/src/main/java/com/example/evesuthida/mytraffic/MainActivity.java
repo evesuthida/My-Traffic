88172b4aff784f9b3f5c411ba12794355e9fadea
suthida <zarasale@gmail.com>
2016-07-31 10:59:13 +0700

    Intent to WebView (urlYoutube)

--- app/src/main/java/com/example/evesuthida/mytraffic/MainActivity.java
+++ app/src/main/java/com/example/evesuthida/mytraffic/MainActivity.java
@@ -1,19 +1,34 @@
 package com.example.evesuthida.mytraffic;
 
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
+    //Explicit
+    private String urlYoutubeString;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    } // Main Method
+    } //Main Method
 
     public void clickAboutMe(View view) {
+        MediaPlayer mediaPlayer = MediaPlayer.create(this, R.raw.effect_btn_shut);
+        mediaPlayer.start();
+
+        //Intent to Webview
+        urlYoutubeString = "https://youtu.be/eNmL4JiGxZQ";
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setData(Uri.parse(urlYoutubeString));
+        startActivity(intent);
+
 
     }
 
-} // Main Class
+} //Main Class
